49a2010313fd987cbd48648f2c0cd877538edf9b
fish2203 <fish2203@gmail.com>
2023-05-30 20:45:00 +0900

    modal-checkAgain

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-
-
 function check_again(){
+  const modal = document.querySelector('.modal');
+  const body = document.querySelector('body');
+  const button = document.querySelector('.button');
 
+  modal.style.visibility = 'visible';
+  button.style.visibility = 'hidden';
+  body.style.backgroundColor = 'gray'
+}
+
+function shutdown_modal(){
+  const modal = document.querySelector('.modal');
+  const body = document.querySelector('body');
+  const button = document.querySelector('.button');
+
+  modal.style.visibility = 'hidden';
+  button.style.visibility = 'visible';
+  body.style.backgroundColor = 'white'
 }
